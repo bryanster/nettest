526122a3a5286ed5f810989533c485b9a26a75fe
Bryan van der Net <bryan@nexezo.com>
2021-01-29 20:35:06 +0100

    update

--- index.js
+++ index.js
@@ -1,28 +1,48 @@
 const isPortReachable = require('is-port-reachable');
+var ping = require('ping');
 var config = require('./config.json')
  
 async function main(){
-    var devices = config.devices
-    devices.forEach((device) => {
-        console.log(`Checking device: ${device.Name}`)
-        var ip = device.ip
-        var service = device.services
-        service.forEach(service => {
-            isPortReachable(service.port, {host: `${ip}`}).then((value) =>{
-                if (value == true) {
-                    console.log(`${service.name} \t= \t Reachable`)
-                }
-                else(
-                    console.log(`${service.name} \t= \tunreachable`)
-                )
-                
-            })
-
-        })
+    await console.log("starting\n")
+    var host = "8.8.8.8"
+    await console.log("testing internet connectivity\n")
+    ping.promise.probe(host).then((response) => {
+        if(response.alive === true){
+            console.log("google\t=\treachable\n")
+        }
+        else(console.log("google\t=\tunreachable\n"))
+        checkdevices();
     })
+    
+
+    
 
 
 }
 
 main()
+
+async function checkdevices() {
+    await console.log("starting internal device checks\n");
+
+    var devices = config.devices;
+    devices.forEach((device) => {
+        console.log(`Checking device: ${device.Name}\n`);
+        var ip = device.ip;
+        var service = device.services;
+        service.forEach(service => {
+            isPortReachable(service.port, { host: `${ip}` }).then((value) => {
+                if (value == true) {
+                    console.log(`${service.name} \t= \t Reachable`);
+                }
+                else
+                    (
+                        console.log(`${service.name} \t= \tunreachable`)
+                    );
+
+            });
+
+        });
+    });
+}
 
