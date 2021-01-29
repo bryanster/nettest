ff1ca8eba3aa2c502e6b7a00d81b0f555edd0b32
Bryan van der Net <bryan@nexezo.com>
2021-01-30 00:32:34 +0100

    style

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const logSymbols = require('log-symbols');
 var config = require('./config.json')
  
 async function main(){
-    await console.log("starting\n")
+    console.log("starting\n")
     var host = "8.8.8.8"
-    await console.log("testing internet connectivity\n")
+    console.log("testing internet connectivity\n")
     ping.promise.probe(host).then((response) => {
         if(response.alive === true){
             console.log(`google\t=\t${logSymbols.success}\n`)
@@ -20,11 +20,12 @@ async function main(){
 main()
 
 async function checkdevices() {
-    await console.log("starting internal device checks\n");
+    console.log("starting internal device checks\n");
     var devices = config.devices;
 
     for (i in devices){
-        await console.log(`Checking device: ${devices[i].Name}`);
+        console.log(`Checking device: ${devices[i].Name}`);
+        console.log(`on: ${devices[i].ip}`)
         var services = devices[i].services;
         for(j in services){
             var value= await isPortReachable(services[j].port, {host: `${devices[i].ip}`})
