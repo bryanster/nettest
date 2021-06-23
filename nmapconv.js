84f7f5083b2ebcb57aac36861f7f2f4a788bcbe2
bryanster <bnet1998@gmail.com>
2021-06-23 23:45:53 +0200

    remove unnescacery files

--- nmapconv.js
+++
@@ -1,7 +0,0 @@
-var processor = require('nmap2json')
-const fs = require('fs')
-var uedxmlnpars = fs.readFileSync("./test.xml")
-
-processor(uedxmlnpars, (err, result)=>{
-  console.log(result.host.ports)
-})
